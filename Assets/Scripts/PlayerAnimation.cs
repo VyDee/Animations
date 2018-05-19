009cd3aaab2bd4d39bfca148ad63f0ed5dcc1dbf
VyDee <vy.duong@cuw.edu>
2018-05-18 21:13:40 -0500

    5/18/18
    new

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour {
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private string SLEEP_END_ANIMATION = "sleep_end";
+    private string IDLE_ANIMATION = "idle";
+    private string WALK_PARAMETER = "Walk";
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Awake ()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    public void PlayerWalk (bool walk)
+    {
+        this.anim.SetBool(WALK_PARAMETER, walk);
+    }
+
+
+    void EndSleep()
+    {
+        this.anim.Play(SLEEP_END_ANIMATION);
+    }
+    void BeginIdle()
+    {
+        this.anim.Play(IDLE_ANIMATION);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PlayerWalk.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+
+    public float rotationSpeed = 3f;
+    private float rotateY;
+    private PlayerAnimation playerAnim;
+
+    private float h, v;
+
+	void Awake ()
+    {
+        playerAnim = GetComponent<PlayerAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckMovement();
+        AnimatePlayer();
+	}
+    void CheckMovement()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        rotateY -= h * rotationSpeed;
+
+        this.transform.localRotation = Quaternion.AngleAxis(rotateY, Vector3.up);
+    }
+    void AnimatePlayer ()
+    {
+        if (v !=0 )
+        {
+            playerAnim.PlayerWalk(true);
+        }
+        else
+        {
+            playerAnim.PlayerWalk(false);
+        }
+
+    }
+}
