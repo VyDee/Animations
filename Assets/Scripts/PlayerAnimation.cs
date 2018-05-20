99687e1230d90b33ccea706fea33a4ddc9652e88
VyDee <38266371+VyDee@users.noreply.github.com>
2018-05-20 01:01:40 -0500

    5/20/18
    n

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -9,12 +9,34 @@ public class PlayerAnimation : MonoBehaviour
     private string SLEEP_END_ANIMATION = "sleep_end";
     private string IDLE_ANIMATION = "idle";
     private string WALK_PARAMETER = "Walk";
+    private string ATTACK_1_PARAMETER = "Attack1";
+    private string ATTACK_2_PARAMETER = "Attack2";
+
 
 	// Use this for initialization
 	void Awake ()
     {
         this.anim = GetComponent<Animator>();
     }
+
+    public void Attack1()
+    {
+        this.anim.SetBool(ATTACK_1_PARAMETER, true);
+    }
+    public void EndAttack1 ()
+    {
+        this.anim.SetBool(ATTACK_1_PARAMETER, false);
+    }
+
+    public void Attack2()
+    {
+        this.anim.SetBool(ATTACK_2_PARAMETER, true);
+    }
+    public void EndAttack2 ()
+    {
+        this.anim.SetBool(ATTACK_2_PARAMETER, false);
+    }
+
 
     public void PlayerWalk (bool walk)
     {
